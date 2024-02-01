4e8a456f173425836e6368ecc026ce6b723314bb
ShadowBlack <daniel.miliczek@op.pl>
2024-02-01 21:50:34 +0100

    step two complete

--- multi-setp-form/src/app/form/components/step-two/step-two.component.ts
+++ multi-setp-form/src/app/form/components/step-two/step-two.component.ts
@@ -9,14 +9,20 @@ export class StepTwoComponent implements OnInit {
   @Output() nextStep = new EventEmitter();
   @ViewChild('periodSelected', { read: ElementRef }) periodSelected!: ElementRef;
 
-  selectedOption: string | undefined;
-  periodOption: boolean | undefined;
+  selectedOption: string = 'arcade';
+  periodOption: boolean = false;
+  
 
   ngOnInit(): void {
     const storedPeriodOption = sessionStorage.getItem('period');
+    const storedSelectedOption = sessionStorage.getItem('selectedOption');
 
     if (storedPeriodOption) {
       this.periodOption = storedPeriodOption === 'true';
+    }
+
+    if (storedSelectedOption) {
+      this.selectedOption = storedSelectedOption;
     }
   }
 
@@ -29,10 +35,13 @@ export class StepTwoComponent implements OnInit {
   }
 
   nextStepClick(): void {
+    const selectedOption: string = this.selectedOption;
+
     const periodOption: boolean = this.periodSelected.nativeElement.checked;
     const periodOptionToString: string = periodOption.toString();
 
     sessionStorage.setItem('period', periodOptionToString);
+    sessionStorage.setItem('selectedOption', selectedOption);
     this.nextStep.emit(3);
   }
 
